e39eb38cfacc853a1300971faf5ba605bd29abef
William Bradley <williambbradley@gmail.com>
2013-06-22 03:00:21 -0700

    some stuff

--- ultimatoe.cpp
+++ ultimatoe.cpp
@@ -14,6 +14,8 @@
 
 typedef int piece_t;
 typedef int player_t;
+struct board_t;
+struct meta_board_t;
 
 struct move_t
 {
@@ -23,9 +25,69 @@ struct move_t
 	bool exists() const { return board >= 0 and space >= 0; }
 };
 
+player_t get_space_owner(const board_t &board, int space);
+player_t get_space_owner(const meta_board_t &meta_board, int board);
+bool all_spaces_owned(const board_t &board);
+bool all_spaces_owned(const meta_board_t &board);
+
+template <typename B>
+player_t game_over(const B &board, player_t &winner, bool &tie)
+{
+	struct 
+	{
+		int a, b, c;
+	} combos[] = {
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8},
+		{0, 3, 6},
+		{1, 4, 7},
+		{2, 5, 8},
+		{0, 4, 8},
+		{2, 4, 6},
+	};
+	for (auto &combo : combos)
+	{
+		auto winner_a = get_space_owner(board, combo.a);
+		if (!winner_a)
+			continue;
+		auto winner_b = get_space_owner(board, combo.b);
+		if (!winner_b || (winner_a != winner_b))
+			continue;
+		auto winner_c = get_space_owner(board, combo.c);
+		if (!winner_c || (winner_b != winner_c))
+			continue;
+		winner = winner_c;
+		tie = false;
+		return true;
+	}
+
+	if (all_spaces_owned(board))
+	{
+		winner = 0;
+		tie = true;
+		return true;
+	}
+
+	return false;
+}
+
 struct board_t
 {
+	board_t()
+   	{
+		memset(&spaces[0], 0, sizeof(piece_t) * 9);
+	}
 	std::tr1::array<piece_t, 9> spaces;
+	
+	player_t winner() const
+	{
+		player_t winner = 0;
+		bool tie = false;
+		game_over(*this, winner, tie);
+		return winner;
+	}
+
 	void clear()
 	{
 		for (auto &space : spaces)
@@ -131,13 +193,39 @@ struct meta_board_t
 
 	bool game_over(player_t &winner, bool &tie) const
 	{
-		assert(0);
-
-		// TODO: compute whether there is a winner
-		return false;
+		return ::game_over(*this, winner, tie);
 	}
 };
 
+player_t get_space_owner(const board_t &board, int space)
+{
+	return board.spaces[space];
+}
+
+player_t get_space_owner(const meta_board_t &meta_board, int board)
+{
+	return meta_board.boards[board].winner();
+}
+
+bool all_spaces_owned(const board_t &board)
+{
+	for (const auto space : board.spaces)
+	{
+		if (!space)
+			return false;
+	}
+	return true;
+}
+
+bool all_spaces_owned(const meta_board_t &meta_board)
+{
+	for (const auto &board : meta_board.boards)
+	{
+		if (!board.winner())
+			return false;
+	}
+	return true;
+}
 
 int main(int argc, char *argv[])
 {
@@ -146,7 +234,18 @@ int main(int argc, char *argv[])
 
 	if (argc == 2 && strcmp(argv[1], "-t") == 0)
 	{
-		std::cout << "asdf" << std::endl;
+		std::cout << "Running ultimatoe tests..." << std::endl;
+
+		// test simple game over cases
+		meta_board_t meta_board;
+		meta_board.boards[0].spaces[0] = X;
+		meta_board.boards[0].spaces[1] = X;
+		meta_board.boards[0].spaces[2] = X;
+		player_t winner = false;
+		bool tie = false;
+		assert(meta_board.boards[0].winner() == X);
+		assert(!meta_board.game_over(winner, tie));
+		assert(!tie);
 		return EXIT_SUCCESS;
 	}
 
